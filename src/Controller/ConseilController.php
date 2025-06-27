624df3ae29a3206fb1bbb19f85bd93198e817bae
Benoit Souillat <dev.souillat@gmail.com>
2025-06-27 13:10:08 +0200

    add put and delete route and groups'getConseils'

--- src/Controller/ConseilController.php
+++ src/Controller/ConseilController.php
@@ -2,25 +2,88 @@
 
 namespace App\Controller;
 
+use App\Entity\Conseil;
 use App\Repository\ConseilRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Validator\Constraints\Json;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api/conseil', name: 'api_conseil_')]
 final class ConseilController extends AbstractController
 {
     public function __construct(
-        public readonly ConseilRepository $repository
+        public readonly ConseilRepository $repository,
+        public readonly UserRepository $userRepository,
+        public readonly EntityManagerInterface $manager,
+        public readonly SerializerInterface $serializer, //Event Dispatcher ??
     ){}
 
     #[Route('', name: 'conseils', methods: ['GET'])]
     public function getAllConseils(): JsonResponse
     {
         $conseils = $this->repository->findAll();
+        $json = $this->serializer->serialize($conseils, 'json');
 
-        return new JsonResponse([
-            'conseils' => $conseils,
-        ]);
+        return new JsonResponse($json, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/{id}', name: 'conseil_show', methods: ['GET'])]
+    public function getConseil(Request $request): JsonResponse
+    {
+        $conseil = $this->repository->find($request->get('id'));
+        $json = $this->serializer->serialize($conseil, 'json', ['groups' => 'getConseils']);
+        return new JsonResponse($json, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/add', name: 'add', methods: ['POST'])]
+    public function addConseil(Request $request, UrlGeneratorInterface $urlGenerator, ValidatorInterface $validator): JsonResponse
+    {
+        $conseil = $this->serializer->deserialize($request->getContent(), Conseil::class, 'json');
+        $errors = $validator->validate($conseil);
+
+        if ($errors->count() > 0) {
+            return new JsonResponse($this->serializer->serialize($errors, 'json'), Response::HTTP_BAD_REQUEST, [], true);
+            //throw new HttpException(JsonResponse::HTTP_BAD_REQUEST, "La requête est invalide");
+        }
+        $user = $request->get('userId') ? $this->userRepository->find($request->get('userId')) : null;
+        $conseil->setUser($user);
+        $this->manager->persist($conseil);
+        $this->manager->flush();
+        $json = $this->serializer->serialize($conseil, 'json', ['groups' => 'getConseils']);
+        $location = $urlGenerator->generate('api_conseil_add', ['id' => $conseil->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($json, Response::HTTP_CREATED, ['Location' => $location], true);
+
+    }
+    #[Route('/{id}', name: 'edit', methods: ['PUT'])]
+    public function editConseil(Request $request, ValidatorInterface $validator): JsonResponse
+    {
+        $conseil = $this->repository->find($request->get('id'));
+        $updatedConseil = $this->serializer->deserialize($request->getContent(), Conseil::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $conseil]);
+        $user = $request->get('userId') ? $this->userRepository->find($request->get('userId')) : null;
+        $conseil->setUser($user);
+        $this->manager->persist($updatedConseil);
+        $this->manager->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
+    public function deleteConseil(Request $request): JsonResponse
+    {
+        $conseil = $this->repository->find($request->get('id'));
+        $this->manager->remove($conseil);
+        $this->manager->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
--- src/Entity/Conseil.php
+++ src/Entity/Conseil.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\ConseilRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ConseilRepository::class)]
 class Conseil
@@ -12,13 +14,24 @@ class Conseil
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['getConseils'])]
     private ?int $id = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Groups(['getConseils'])]
+    #[Assert\NotBlank(message: "La date (le mois) du conseil est obligatoire.")]
     private ?\DateTime $date = null;
 
+    #[Assert\NotBlank(message: "Le contenu du conseil est obligatoire.")]
+    #[Assert\Length(min: 1, minMessage:  "Le contenu du conseil est trop court.")]
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['getConseils'])]
     private ?string $description = null;
+
+    #[ORM\ManyToOne(inversedBy: 'conseils')]
+    #[ORM\JoinColumn(nullable: true)]
+    private ?User $User = null;
+
 
     public function getId(): ?int
     {
@@ -45,6 +58,18 @@ class Conseil
     public function setDescription(string $description): static
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->User;
+    }
+
+    public function setUser(?User $User): static
+    {
+        $this->User = $User;
 
         return $this;
     }
